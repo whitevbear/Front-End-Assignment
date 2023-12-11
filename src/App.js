54f85b916a7406dffa4ee335b7ffbabc1a79be38
whitevbear <whitevbear@gmail.com>
2023-12-11 21:08:05 +0800

    導入HTML 與 CSS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
+import dog01 from './dog01.png';
 import './App.css';
+import './appindex.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        猜數字遊戲
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="message"></p>
+        <section name="submitArea" id="submitArea" className="submitArea">
+          <input type="text" id="guessText" className="guessText"/>
+          <input type="submit" value="猜!" id="guessSubmit" className="guessSubmit"/>
+        </section>
+
+        <p className="answer">
+        <button type="button" id="answerButton" className="answerButton"> 
+          <img src={dog01} 
+          id="answerButtonimg" 
+          className="answerButtonimg" alt="dog01.png" />
+          <p className="theanswer">( つ•̀ω•́)つ答案揭曉</p></button>
+        </p>
+
       </header>
     </div>
   );
